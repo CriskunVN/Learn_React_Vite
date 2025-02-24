e2a56b979bd87e0ff19991a952aab4469793fd30
CriskunVN <phukun0404@gmail.com>
2025-02-24 19:37:53 +0700

    #36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,10 +1,27 @@
 const TodoNew = (props) => {
   const { addNewData } = props;
   // addNewData("PhuKun");
+
+  const handleClick = () => {
+    addNewData("PhuKun");
+    // alert("click me");
+  };
+
+  const handleChange = (name) => {
+    console.log(name);
+  };
   return (
     <div className="todo-input">
-      <input type="text" placeholder="Add a todo..." />
-      <button>Add</button>
+      <input
+        onChange={(e) => {
+          handleChange(e.target.value);
+        }}
+        type="text"
+        placeholder="Add a todo..."
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
